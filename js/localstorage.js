1f9433c34d8733f805842cc179d8ec39c70a7ed6
Geraldine Lagarrigue <lagarriguegeraldine@gmail.com>
2026-06-16 19:49:30 -0300

    LocalStorage

---
+++ js/localstorage.js
@@ -0,0 +1,45 @@
+localStorage.setItem(
+    "Usuarios",
+    JSON.stringify([
+        {
+            "id": 1,
+            "usuario": "ClaraOrnella",
+            "email": "clari.ornella@gmail.com",
+            "password": "12345678",
+            "rol": "admin",
+            "estado": "activo"
+        },
+        {
+            "id": 2,
+            "usuario": "GeraldineLagarrigue",
+            "email": "lagarriguegeraldine@gmail.com",
+            "password": "12345678",
+            "rol": "admin",
+            "estado": "activo"
+        },
+        {
+            "id": 3,
+            "usuario": "Cliente1",
+            "email": "cliente1@gmail.com",
+            "password": "12345678",
+            "rol": "cliente",
+            "estado": "activo"
+        },
+        {
+            "id": 4,
+            "usuario": "Cliente2",
+            "email": "cliente2@gmail.com",
+            "password": "12345678",
+            "rol": "cliente",
+            "estado": "activo"
+        },
+        {
+            "id": 5,
+            "usuario": "Cliente3",
+            "email": "cliente3@gmail.com",
+            "password": "12345678",
+            "rol": "cliente",
+            "estado": "inactivo"
+        }
+    ])
+)
